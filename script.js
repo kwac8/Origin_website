89a3e2f8198d1e7b303f98522e76cf458c2a87e6
kwac8 <elgmato@gmail.com>
2026-05-22 14:35:30 +0200

    Initial upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navbarLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+
+navLinks.classList.toggle('active');
+hamburger.classList.toggle('is-active');
+});
