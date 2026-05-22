de26a811a31451eddfd9278dd185540c40a5c7d2
kwac8 <elgmato@gmail.com>
2026-05-22 14:50:16 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navbarLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-
-navLinks.classList.toggle('active');
-hamburger.classList.toggle('is-active');
-});
